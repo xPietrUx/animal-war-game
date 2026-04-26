f7025ab4811bf15463f4043612601911d3ce2165
Piotr Dusinski <piotdusinski01@gmail.com>
2026-04-26 14:28:50 +0200

    Merge: conflict resolved

--- Assets/JungleLaw/TourSystem/TurnManager.cs
+++ Assets/JungleLaw/TourSystem/TurnManager.cs
@@ -107,8 +107,6 @@ public class TurnManager : MonoBehaviour
             FogOfWarManager.Instance.UpdateFog(currentTurnTeam);
         }
 
-        // Obliczamy numer drużyny (Player1 = 1, Player2 = 2)
-        int currentTurnTeam = (currentTurn == TurnState.Player1) ? 1 : 2;
 
         // Zaktualizuj mgłę dla nowego gracza!
         if (FogOfWarManager.Instance != null)
